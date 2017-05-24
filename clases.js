0431eee91257149adda669307214f90826041f34
Carlos Augusto <carlosflores@motumweb.com>
2017-05-24 23:41:15 +0000

    Se crea metodo de saludos

--- clases.js
+++ clases.js
@@ -1,10 +1,15 @@
 var Persona = (function () {
     function Persona(nom, ape) {
-        console.log(nom);
-        console.log(ape);
+        this.nombre = nom;
+        this.apellidos = ape;
     }
+    Persona.prototype.getSaludo = function () {
+        var carita = 'ojo';
+        return "Saludos\n        " + this.nombre + ", " + this.apellidos + "\n        Te enviamos saludos desde consola\n        " + carita + "\n        ";
+    };
     return Persona;
 }());
 var per1 = new Persona();
 var per2 = new Persona('carlos');
 var per3 = new Persona('carlos', 'flores');
+console.log(per3.getSaludo());
--- clases.ts
+++ clases.ts
@@ -3,11 +3,21 @@ class Persona{
     apellidos:string;
 
     constructor(nom?:string,ape?:string){
-        console.log(nom);
-        console.log(ape);
+        this.nombre = nom;
+        this.apellidos = ape;
+    }
+
+    getSaludo():string{
+        let carita = 'ojo';
+        return `Saludos
+        ${this.nombre}, ${this.apellidos}
+        Te enviamos saludos desde consola
+        ${carita}
+        `;
     }
 }
 
 let per1 = new Persona();
 let per2 = new Persona('carlos');
 let per3 = new Persona('carlos','flores');
+console.log(per3.getSaludo());
